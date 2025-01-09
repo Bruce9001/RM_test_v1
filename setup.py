4f0a84ffe3c7424de0f9f96e6dc8f668d2e651c1
Bruce9001 <bruce.kim@gbike.io>
2025-01-09 22:16:12 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="RM_test_v1",
     version="1.0.0",
     author="SJKIM",
@@ -9,15 +9,13 @@ setuptools.setup(
     long_description="RM_test_v1",
     long_description_content_type="text/markdown",
     url="https://github.com/Bruce9001/RM_test_v1.git",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     install_requires=[
         "datetime"
-
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
     python_requires='>=3.8',
-
 )
