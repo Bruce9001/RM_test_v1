211220b13d6f341f55c8f40cf36b4582ba4e2d05
Bruce9001 <bruce.kim@gbike.io>
2025-01-10 14:21:54 +0900

    update for runing test

--- RM_test_v1/__init__.py
+++ RM_test_v1/__init__.py
@@ -1,5 +1,14 @@
+import mysql.connector
+import pandas
 import datetime
 from datetime import timedelta
+import psycopg2
+import numpy
 from dateutil.relativedelta import relativedelta
+from statsmodels.tsa.seasonal import seasonal_decompose
+from sklearn.ensemble import IsolationForest
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
-__all__ = ["datetime", "relativedelta"]
+__all__ = ["datetime", "relativedelta", "mysql", "pandas", "numpy", "psycopg2", "seasonal_decompose", "IsolationForest", "px", "go", "make_subplots","DATA_EXT","DATA_EDA_SHOW"]
--- setup.py
+++ setup.py
@@ -11,20 +11,25 @@ setup(
     url="https://github.com/Bruce9001/RM_test_v1.git",
     packages=find_packages(),
     install_requires=[
-        "Datetime",
-        "pandas",
-        "mysql",
-        "numpy",
-        "python-dateutil",
-        "psycopg2",
-        "statsmodels",
-        "scikit-learn",
-        "plotly"
-
+        "datetime>=5.4",
+        "pandas>=2.0.0",
+        "mysql-connector-python>=8.0.0",
+        "numpy>=1.24.0",
+        "python-dateutil>=2.8.2",
+        "psycopg2-binary>=2.9.0",
+        "statsmodels>=0.14.0",
+        "scikit-learn>=1.3.0",
+        "plotly>=5.13.0"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules"
     ],
     python_requires='>=3.8',
+    include_package_data=True,
+    zip_safe=False
 )
